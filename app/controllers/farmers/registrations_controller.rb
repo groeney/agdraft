9be2912ea88cef0147bd711d5eb3c9f3bddb5abd
Xander <xander.groeneveld@gmail.com>
2016-09-07 13:19:20 +0700

    [FEATURE] Edit Account Details for Farmer and Worker

--- app/controllers/farmers/registrations_controller.rb
+++ app/controllers/farmers/registrations_controller.rb
@@ -1,7 +1,20 @@
 class Farmers::RegistrationsController < Devise::RegistrationsController
+  before_filter :dashboard_type, only: [:edit, :update]
+  before_filter :nav_item, only: [:edit, :update]
+  layout "dashboard", only: [:edit, :update]
+
+  protected
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
     farmer_dashboard_path
+  end
+
+  def dashboard_type
+    @dashboard_type = "farmers"
+  end
+
+  def nav_item
+    @nav_item = "account_details"
   end
 end
--- app/controllers/workers/registrations_controller.rb
+++ app/controllers/workers/registrations_controller.rb
@@ -1,4 +1,9 @@
 class Workers::RegistrationsController < Devise::RegistrationsController
+  before_filter :dashboard_type, only: [:edit, :update]
+  before_filter :nav_item, only: [:edit, :update]
+  layout "dashboard", only: [:edit, :update]
+  
+  protected
   # The path used after sign up.
   def after_sign_up_path_for(resource)
     worker_onboard_job_categories_path
@@ -7,5 +12,13 @@ class Workers::RegistrationsController < Devise::RegistrationsController
   # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
     worker_onboard_job_categories_path
+  end
+
+  def dashboard_type
+    @dashboard_type = "workers"
+  end
+
+  def nav_item
+    @nav_item = "account_details"
   end
 end
