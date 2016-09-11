7a81a1c658165fb7948a75d0cea6db31098e6a73
Xander <xander.groeneveld@gmail.com>
2016-09-11 17:01:35 +0700

    [PATCH] Added sendgrid config to heroku apps for now to enable signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,14 @@ Rails.application.configure do
       s3_region: ENV.fetch('AWS_REGION'),
     }
   }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
