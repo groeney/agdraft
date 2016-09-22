85f51a36959516241d6be58b44dcbb8dbf0c02b2
Xander <xander.groeneveld@gmail.com>
2016-09-22 16:58:34 +0800

    [PATCH] Bug on worker account details

--- app/controllers/workers/registrations_controller.rb
+++ app/controllers/workers/registrations_controller.rb
@@ -1,10 +1,24 @@
 class Workers::RegistrationsController < Devise::RegistrationsController
   before_filter :dashboard_type, only: [:edit, :update]
   before_filter :nav_item, only: [:edit, :update]
-  layout "dashboard", only: [:edit, :update]
-  layout "hero", only: [:new, :create]
-  
+  layout :determine_layout  
+
   protected
+  # cannot specify two different 'layout' options at the top of the controller 
+  # hence needing to use this method to determine appropriate layout
+  def determine_layout
+    case action_name
+    when "new"
+      "hero"
+    when "create"
+      "hero"
+    when "edit"
+      "dashboard"
+    when "update"
+      "dashboard"
+    end
+  end
+  
   # The path used after sign up.
   def after_sign_up_path_for(resource)
     worker_onboard_job_categories_path
