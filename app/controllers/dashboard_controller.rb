a913c3898ab726b16b0a54968b5fd9119a468bd6
James Groeneveld <jamesgroeneveld@gmail.com>
2016-10-17 13:40:55 +1000

    [FEATURE] Adding a top menu bar to dashboard pages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,17 @@
 class DashboardController < ApplicationController
   before_filter :dashboard_type
   before_filter :nav_item
-  layout 'dashboard'
+  before_filter :notifications
+  layout "dashboard"
 
   private
 
   def nav_item
+  end
+
+  def notifications
+    if (current_resource = current_farmer || current_worker)
+      @unseen_notifications = current_resource.notifications.where({ unseen: true }).exists?
+    end
   end
 end
--- app/controllers/workers/overview_controller.rb
+++ app/controllers/workers/overview_controller.rb
@@ -2,6 +2,5 @@ class Workers::OverviewController < Workers::BaseController
   def index
     flash[:alert] = "<div class='header'>Unverified Account</div><p>You're account is not currently verified which decreases your chance of being matched with jobs. In order for us to verify your account we need to do a reference check. Please create a Previous Employer record by clicking on the link in the side bar. Once created, it will take us 3-5 buiness days to verify the information provided</p>" if !current_worker.verified
     @recommended_jobs = current_worker.recommended_jobs
-    @unseen_notifications = current_worker.notifications.where({ unseen: true }).exists?
   end
 end
