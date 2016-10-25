06bf2c2405b619a9ab439ebe9d934c6add60b174
Xander <xander.groeneveld@gmail.com>
2016-10-25 22:37:11 +1000

    [BUG] js bug fixed

--- app/assets/javascripts/workers/extra_details.js
+++ app/assets/javascripts/workers/extra_details.js
@@ -13,20 +13,19 @@ $(document).on('turbolinks:load', function(){
     }).on('ajax:error',function(e, xhr, status, error){
       toastr['error'](xhr.responseJSON.error);
     });
-  });
+    $('#edit-worker .worker_visa select').on("change", function(){
+      if(this.value == 'Working Holiday Visa' || this.value == 'Tourist Visa'){
+        $('#edit-worker .worker_visa_number').show()
+        $('#edit-worker .worker_visa_number input').focus();
+      }else{
+        $('#edit-worker .worker_visa_number').hide();
+      }
+    });
 
-  $('#edit-worker .worker_visa select').on("change", function(){
-    if(this.value == 'Working Holiday Visa' || this.value == 'Tourist Visa'){
+    // Display hidden visa number on page load if applicable
+    var visa = $('#edit-worker .worker_visa select')[0].value
+    if(visa == 'Working Holiday Visa' || visa == 'Tourist Visa'){
       $('#edit-worker .worker_visa_number').show()
-      $('#edit-worker .worker_visa_number input').focus();
-    }else{
-      $('#edit-worker .worker_visa_number').hide();
     }
   });
-
-  // Display hidden visa number on page load if applicable
-  var visa = $('#edit-worker .worker_visa select')[0].value
-  if(visa == 'Working Holiday Visa' || visa == 'Tourist Visa'){
-    $('#edit-worker .worker_visa_number').show()
-  }
 });
