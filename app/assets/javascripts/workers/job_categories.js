55a80b37def30982cba7d2e442f941b8ceff83dd
James Groeneveld <jamesgroeneveld@gmail.com>
2016-10-25 19:18:54 +0200

    [FEATURE] redesign of the job/skill picker for onboard
    Note: job/skill picker from dashboard will redirect to onboard  flow

--- app/assets/javascripts/workers/job_categories.js
+++ app/assets/javascripts/workers/job_categories.js
@@ -1,10 +1,19 @@
 $(document).on('turbolinks:load', function(){
   $('.onboard.job_categories, .job_categories.index').ready(function(){
-    var activeClass = 'red';
-    $('.ui.link.card.job-category').on('click', function() {
+    $('.image.job-category').popup({
+      inline: true,
+      on: 'click',
+      position: 'bottom center',
+      onShow: function($module){
+        if ($($module).data('selected')){
+          return false
+        }
+      }
+    });
+
+    $('.job-category').on('click', function() {
       var jobCategoryID = $(this).data('id');
-      var selected = $(this).data('selected');
-      if ($(this).hasClass(activeClass)){
+      if ($(this).data('selected')){
         $.ajax({
           context: this,
           url: Routes.worker_job_category_path(jobCategoryID),
@@ -15,11 +24,12 @@ $(document).on('turbolinks:load', function(){
           contentType: 'application/json',
           dataType: 'json',
           success: function(data, textStatus, jqXHR){
-            
+            $(this).data('selected', false);
+            $(this).removeClass('selected');
+            $('.item[data-job-category-id=' + jobCategoryID + ']').css('display', 'none');
           },
           error: function(jqXHR, textStatus, errorThrown){
             toastr['error'](errorThrown);
-            $(this).toggleClass(activeClass);
           }
         })
       } else {
@@ -36,7 +46,56 @@ $(document).on('turbolinks:load', function(){
             id: jobCategoryID
           }),
           success: function(data, textStatus, jqXHR){
-            
+            $(this).data('selected', true);
+            $(this).addClass('selected');
+            $('.item[data-job-category-id=' + jobCategoryID + ']').css('display', 'block');
+          },
+          error: function(jqXHR, textStatus, errorThrown){
+            toastr['error'](errorThrown);
+          }
+        });
+      }
+    });
+
+    $('.ui.label.skill').on('click', function () {
+      var skillID = $(this).data('id');
+      if ($(this).data('selected')){
+        $.ajax({
+          context: this,
+          url: Routes.worker_skill_path(skillID),
+          method: 'DELETE',
+          accepts: {
+            json: 'application/json'
+          },
+          contentType: 'application/json',
+          dataType: 'json',
+          success: function(data, textStatus, jqXHR){
+            $(this).removeClass('teal');
+            $(this).data('selected', false);
+            $('.item[data-skill-id=' + skillID + ']').css('display', 'none');
+          },
+          error: function(jqXHR, textStatus, errorThrown){
+            toastr['error'](errorThrown);
+            $(this).toggleClass(activeClass);
+          }
+        });
+      }else{
+        $.ajax({
+          context: this,
+          url: Routes.worker_skills_path(),
+          method: 'POST',
+          accepts: {
+            json: 'application/json'
+          },
+          contentType: 'application/json',
+          dataType: 'json',
+          data: JSON.stringify({
+            id: skillID
+          }),
+          success: function(data, textStatus, jqXHR){
+            $(this).addClass('teal');
+            $(this).data('selected', true);
+            $('.ui.label[data-skill-id=' + skillID + ']').css('display', 'inline-block');
           },
           error: function(jqXHR, textStatus, errorThrown){
             toastr['error'](errorThrown);
@@ -44,7 +103,6 @@ $(document).on('turbolinks:load', function(){
           }
         });
       }
-      $(this).toggleClass(activeClass);
     });
   });
 });
