cfd583b5c1e13a35113be89493e0c7c1990ac2a6
James Groeneveld <jamesgroeneveld@gmail.com>
2016-10-30 18:27:34 +0100

    [FEATURE] job page redesign

--- app/assets/javascripts/pages/job_show.js
+++ app/assets/javascripts/pages/job_show.js
@@ -1,15 +1,17 @@
 $(document).on('turbolinks:load', function(){
   $('.jobs.show').ready(function(){
     $('.rating').rating('disable');
+    $('.tabular.menu .item').tab();
     jobID = /^\/jobs\/[0-9]*/.exec(window.location.pathname)[0].split('/')[2]
-    $('#apply-for-job').on('click', function(){
+    $('.apply-for-job').on('click', function(){
       var $button = $(this);
-      $button.addClass('disabled');
+      $('.apply-for-job').addClass('disabled'); // more than one button on page
       $.ajax({
         url: Routes.worker_express_interest_path(jobID),
         method: 'POST',
         success: function(data, status){
           toastr.success('You\'ve successfully applied to this job');
+          $('.apply-for-job').text('Applied');
         },
         error: function(jqXHR, textStatus, errorThrown){
           $button.removeClass('disabled');
--- app/controllers/farmers_controller.rb
+++ app/controllers/farmers_controller.rb
@@ -1,5 +1,7 @@
 class FarmersController < ApplicationController
+  impressionist
+  layout "hero"
   def show
     @farmer = Farmer.find(params[:id])
   end
-end 
+end
