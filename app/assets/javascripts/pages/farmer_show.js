80424d3c8fee65e0a1a314c13fcd2f920fa21182
James Groeneveld <jamesgroeneveld@gmail.com>
2016-10-30 14:05:59 +0100

    [FEATURE] farmer page redesign

---
+++ app/assets/javascripts/pages/farmer_show.js
@@ -0,0 +1,23 @@
+$(document).on('turbolinks:load', function(){
+  $('.farmers.show').ready(function(){
+    $('.ui.rating').rating('disable');
+    $('.tabular.menu .item').tab();
+    $('.apply-for-job').on('click', function(e){
+      e.preventDefault();
+      var $button = $(this);
+      var jobID = $button.data('job-id');
+      $button.addClass('disabled');
+      $.ajax({
+        url: Routes.worker_express_interest_path(jobID),
+        method: 'POST',
+        success: function(data, status){
+          toastr.success('You\'ve successfully applied to this job');
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+          $button.removeClass('disabled');
+          toastr.error(errorThrown);
+        }
+      });
+    });
+  });
+});
