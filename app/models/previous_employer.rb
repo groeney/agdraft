79558bf0c8caf1cf70e968966e2e226338076508
James Groeneveld <jamesgroeneveld@gmail.com>
2016-11-09 13:40:19 +0100

    [PATCH] syntax error

--- app/models/previous_employer.rb
+++ app/models/previous_employer.rb
@@ -8,12 +8,13 @@ class PreviousEmployer < ActiveRecord::Base
   validates :start_date, presence: true, date: { before: :end_date }, on: [:update, :create]
 
   after_create :email_contact
+  after_create :analytics
 
   protected
 
   def email_contact
     EmailService.new.send_email(Rails.application.config.smart_email_ids[:contact_previous_employer_for_review], contact_email, {worker_name: worker.full_name, job_title: job_title, business_name: business_name, start_date: start_date.strftime("%B %e %Y"), end_date: end_date.strftime("%B %e %Y"), reply_url: previous_employer_review_url(id)}) if contact_email
-  after_create :analytics
+  end
 
   def analytics
     Analytics.track(
