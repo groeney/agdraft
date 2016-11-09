2bc58ae25dce7e50fa3d57b0ac1170aeb8d7a678
James Groeneveld <jamesgroeneveld@gmail.com>
2016-11-09 13:23:02 +0100

    [CLEANUP] design tweaks III

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ActiveRecord::Base
   belongs_to :resource, polymorphic: true
-  has_attached_file :thumbnail, :styles => { :display => "200x200#" }, :default_url => "/assets/blank_user_photo.png"
+  has_attached_file :thumbnail, :styles => { :display => "200x200#" }, :default_url => "/assets/agdraft-logo.png"
   validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
 end
