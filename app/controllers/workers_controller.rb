b38e0ce129641e7f7f43886d59d380602c33b031
Xander <xander.groeneveld@gmail.com>
2016-11-22 16:12:55 -0800

    [PATCH] Bug on worker profile page

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -2,8 +2,10 @@ class WorkersController < ApplicationController
   layout "hero"
   def show
     @worker = Worker.find(params[:id])
-    @jobs = current_farmer.jobs_for_worker(@worker.id) if current_farmer
-    @jobs_applied_to = @jobs.select{|j| j[:invited]}
-    @jobs_not_applied_to = @jobs.select{|j| !j[:invited]}
+    if current_farmer
+      @jobs = current_farmer.jobs_for_worker(@worker.id)
+      @jobs_applied_to = @jobs.select{|j| j[:invited]}
+      @jobs_not_applied_to = @jobs.select{|j| !j[:invited]}
+    end
   end
 end
