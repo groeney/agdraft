ba41c32d533aef8694155ea9becfc73551accf5e
Xander <xander.groeneveld@gmail.com>
2016-11-22 16:06:55 -0800

    [FEATURE] Signin for old users

--- app/controllers/farmers/sessions_controller.rb
+++ app/controllers/farmers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Farmers::SessionsController < Devise::SessionsController
+  before_filter :old_user, only: [:create]
 
   def after_sign_in_path_for(resource)
     Analytics.track(
@@ -9,4 +10,14 @@ class Farmers::SessionsController < Devise::SessionsController
     farmer_dashboard_path
   end
 
+  protected
+
+  def old_user
+    @farmer = Farmer.find_by_email(params["farmer"]["email"])
+    # Is this an old user who has not yet signed in to the new app?
+    if @farmer && @farmer.last_sign_in_at.nil?
+      @farmer.send_reset_password_instructions
+      return render "includes/welcome_to_new_app", layout: "onboard"
+    end
+  end
 end
--- app/controllers/workers/sessions_controller.rb
+++ app/controllers/workers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Workers::SessionsController < Devise::SessionsController
+  before_filter :old_user, only: [:create]
 
   def after_sign_in_path_for(resource)
     Analytics.track(
@@ -6,6 +7,17 @@ class Workers::SessionsController < Devise::SessionsController
       event: "Signin"
       )
     worker_dashboard_path
+  end
+
+  protected
+
+  def old_user
+    @worker = Worker.find_by_email(params["worker"]["email"])
+    # Is this an old user who has not yet signed in to the new app?
+    if @worker && @worker.last_sign_in_at.nil?
+      @worker.send_reset_password_instructions
+      return render "includes/welcome_to_new_app", layout: "onboard"
+    end
   end
 
 end
