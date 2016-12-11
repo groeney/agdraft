a18099468fc0ce3839e38f68cf61d267e1140ba3
Xander <xander.groeneveld@gmail.com>
2016-12-11 22:22:00 +1100

    [FEATURE] Extended the worker profile page to display more info

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -2,6 +2,7 @@ class WorkersController < ApplicationController
   layout "hero"
   def show
     @worker = Worker.find(params[:id])
+    @recent_jobs = JobWorker.where(worker_id: @worker.id, state: "hired")
     if current_farmer
       @jobs = current_farmer.jobs_for_worker(@worker.id)
       @jobs_applied_to = @jobs.select{|j| j[:invited]}
--- app/models/education.rb
+++ app/models/education.rb
@@ -5,4 +5,14 @@ class Education < ActiveRecord::Base
   validates_presence_of :school
   validates :end_date, presence: true, date: { before: Proc.new { Date.tomorrow }, message: "Must end education before at least #{(Date.tomorrow).to_s}" }, on: [:update, :create]
   validates :start_date, presence: true, date: { before: :end_date }, on: [:update, :create]
+
+  def start_date_label
+    return "" unless start_date
+    start_date.strftime("%B %e %Y")
+  end
+
+  def end_date_label
+    return "" unless end_date
+    end_date.strftime("%B %e %Y")
+  end
 end
