56fc147e2fb4661c0c169c59980644ee32914b5a
James Groeneveld <jamesgroeneveld@gmail.com>
2016-12-14 11:14:45 +0000

    [PATCH] recommendation bug

--- app/models/concerns/recommendable.rb
+++ app/models/concerns/recommendable.rb
@@ -4,7 +4,7 @@ module Recommendable
   module ClassMethods
     def recommend(filter_params, except = [], size = 5)
       results = self.filter_or(filter_params)
-      results |= self.all if results.count < size
+      results |= self.visibles if results.count < size
       results.uniq.select { |r| !except.include?(r.id) }.first(size)
     end
   end
