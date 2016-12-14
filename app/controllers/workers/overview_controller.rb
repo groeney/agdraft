049fb1bac8a03ec7f34e1ee14015356d434fd36c
Xander <xander.groeneveld@gmail.com>
2016-12-14 23:51:01 +1100

    [PATCH] Added link to alert

--- app/controllers/workers/overview_controller.rb
+++ app/controllers/workers/overview_controller.rb
@@ -1,7 +1,7 @@
 class Workers::OverviewController < Workers::BaseController
   def index
     if !current_worker.verified
-      flash[:alert] = "<div class='header'>Unverified Account</div><p>You're account is not currently verified which decreases your chance of being matched with jobs. In order for us to verify your account we need to do a reference check. Please create a Previous Experience record by clicking on the link in the side bar. Once created, we will verify the information provided</p>"
+      flash[:alert] = "<div class='header'>Unverified Account</div><p>You're account is not currently verified which decreases your chance of being matched with jobs. In order for us to verify your account we need to do a reference check. Please create a <a href='/worker/previous_employers'>Previous Experience record</a> by clicking on the link in the side bar. Once created, we will verify the information provided</p>"
     end
     @recommendations = current_worker.job_recommendations
     @notifications = current_worker.notifications.where({ unseen: true }).reverse
