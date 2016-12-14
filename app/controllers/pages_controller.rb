f7be552da163b2a180f913ac96101d22a88cf570
James Groeneveld <jamesgroeneveld@gmail.com>
2016-12-14 10:46:07 +0000

    [PATCH] fixing featured workers and jobs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   layout "hero"
 
   def home
-    @workers = Worker.all.sample(8)
-    @jobs = Job.all.sample(8)
+    @workers = Worker.visibles.sample(8)
+    @jobs = Job.visibles.sample(8)
     return redirect_to worker_dashboard_path if current_worker
     return redirect_to farmer_dashboard_path if current_farmer
     return redirect_to admin_dashboard_path if current_admin
