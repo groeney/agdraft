6ab94950190e8b765b8d689d781e5fa7fe42b3fd
James Groeneveld <jamesgroeneveld@gmail.com>
2017-03-02 14:51:50 +0100

    [BUG] worker js not firing due to missing turbolinks

--- app/assets/javascripts/pages/worker_show.js
+++ app/assets/javascripts/pages/worker_show.js
@@ -1,30 +1,32 @@
-$('.workers.show').ready(function(){
-  $('.ui.rating').rating('disable');
-  $('.tabular.menu .item').tab();
-  workerID = /^\/workers\/[0-9]*/.exec(window.location.pathname)[0].split("/")[2]
-  
-  $('.invite-worker').on('click', function(e){
-    $('#invite').modal('show');
-  });
+$(document).on('turbolinks:load', function(){
+  $('.workers.show').ready(function(){
+    $('.ui.rating').rating('disable');
+    $('.tabular.menu .item').tab();
+    workerID = /^\/workers\/[0-9]*/.exec(window.location.pathname)[0].split("/")[2]
 
-  $('.manage-applications').on('click', function(e){
-    $('#manage-applications').modal('show');
-  });
+    $('.invite-worker').on('click', function(e){
+      $('#invite').modal('show');
+    });
 
-  $('.js-invite-worker').on('click', function(){
-    $(this).addClass('disabled');
-    $(this).text('Invited');
-    $.ajax({
-      url: Routes.farmer_invite_worker_path($(this).data('job-id')),
-      method: 'POST',
-      data: { worker_id: workerID },
-      success: function(data, status){
-        _toastr('success', 'Worker invited to job!');
-      },
-      error: function(jqXHR, textStatus, errorThrown){
-        $(this).removeClass('disabled');
-        _toastr('error', errorThrown);
-      }
+    $('.manage-applications').on('click', function(e){
+      $('#manage-applications').modal('show');
+    });
+
+    $('.js-invite-worker').on('click', function(){
+      $(this).addClass('disabled');
+      $(this).text('Invited');
+      $.ajax({
+        url: Routes.farmer_invite_worker_path($(this).data('job-id')),
+        method: 'POST',
+        data: { worker_id: workerID },
+        success: function(data, status){
+          _toastr('success', 'Worker invited to job!');
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+          $(this).removeClass('disabled');
+          _toastr('error', errorThrown);
+        }
+      });
     });
   });
 });
